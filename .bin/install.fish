0b79c45dcca2695d986fbc1bacff16dffdea5446
NI57721 <104410688+NI57721@users.noreply.github.com>
2022-08-16 15:45:34 +0900

    Add a function creating symbolic links

--- .bin/install.fish
+++ .bin/install.fish
@@ -1,14 +1,27 @@
 #!/usr/bin/env fish
 
-set project_root (status dirname)/..
+set project_root (realpath (dirname (status dirname)))
 set allowed_files .vimrc .vim/rc/dein.toml .vim/rc/dein_lazy.toml .config/fish/config.fish .config/fish/fish_plugins .gitignore_global
 
 function backup
   set dir (date "+%Y%m%d-%H%M%S")
   for f in $allowed_files
-    set filepath $project_root/.backup/$dir/$f
-    mkdir -p (dirname $filepath)
-    cp ~/$f $filepath
+    set dirpath $project_root/.backup/$dir/
+    mkdir -p $dirpath
+    and mv ~/$f $dirpath
+    and echo mv ~/$f $dirpath
   end
 end
+
+function create_links
+  for f in $allowed_files
+    set filepath $project_root/$f
+    and ln -snf $filepath ~/$f
+    and echo ln -snf $filepath ~/$f
+  end
+end
+
+
+backup
+and create_links
 
