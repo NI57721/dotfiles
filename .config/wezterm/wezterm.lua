5a03e6aa77d9de591f4829e42db183e412d538d2
NI57721 <104410688+NI57721@users.noreply.github.com>
2023-08-14 12:03:23 +0900

    Change the color scheme of WezTerm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ return {
   font = wezterm.font("HackGenConsoleNF"),
   use_ime = false,
   font_size = 20.0,
-  color_scheme = "Darcura",
+  color_scheme = "Dracula",
   adjust_window_size_when_changing_font_size = false,
   warn_about_missing_glyphs = false,
   enable_csi_u_key_encoding = true,
