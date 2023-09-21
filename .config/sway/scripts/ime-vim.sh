dad63219dd580b22146813d32c5f8dd9e1f191ad
NI57721 <NI57721@users.noreply.github.com>
2023-09-21 23:40:35 +0900

    Add the opacity of the window of Vim ime

--- .config/sway/scripts/ime-vim.sh
+++ .config/sway/scripts/ime-vim.sh
@@ -3,7 +3,7 @@
 FloatingVim=$(swaymsg -t get_tree | jq -c '.. | .floating_nodes? | arrays[] | select(.app_id=="FloatingVim")')
 
 if [ -z $FloatingVim ]; then
-  wezterm --config initial_rows=20 --config initial_cols=60 --config enable_tab_bar=false --config window_background_opacity=0.4 --config text_background_opacity=0.4 start --class FloatingVim fish
+  wezterm --config initial_rows=20 --config initial_cols=60 --config enable_tab_bar=false --config window_background_opacity=0.4 --config text_background_opacity=0.7 start --class FloatingVim fish
 else
   if [ "$(echo $FloatingVim | jq .focused)" = 'true' ]; then
     swaymsg "move window to scratchpad"
