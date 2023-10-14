82a8e0f8caee9bf6b0440c8f18475e23aa089f10
NI57721 <NI57721@users.noreply.github.com>
2023-10-15 04:04:21 +0900

    Modify the names of screenshots

--- .config/sway/scripts/recorder.sh
+++ .config/sway/scripts/recorder.sh
@@ -2,7 +2,7 @@
 
 if [ -z $(ps aux | grep -v grep | grep wf-recorder) ]; then
   notify-send -t 1000 "Start recording"
-  wf-recorder -g "$(slurp)" -F fps=30 -c gif -f /tmp/$(date +%s%N).gif
+  wf-recorder -g "$(slurp)" -F fps=30 -c gif -f $HOME/$(date "+%Y%m%d-%H%M%S-%N").gif
 else
   killall -s SIGINT -e wf-recorder
   notify-send -t 1000 "Stop recording"
