02b2f8fb5f89cfb917822904858eacbedca38b1a
NI57721 <NI57721@users.noreply.github.com>
2023-10-17 07:47:38 +0900

    Fix bugs when cloning Vim repository

--- scripts/update_vim.sh
+++ scripts/update_vim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -u
 
-vim_root=$VIMRUNTIME/..
+vim_root=$XDG_DATA_HOME/vim
 mkdir -p $vim_root
-if [ $(git -C $vim_root rev-parse --is-inside-work-tree 2>/dev/null) == true ]; then
+if [ "$(git -C $vim_root rev-parse --is-inside-work-tree 2>/dev/null)" = true ]; then
   git -C $vim_root pull
 else
   git clone https://github.com/vim/vim.git $vim_root
