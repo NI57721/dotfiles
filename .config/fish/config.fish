ea6908b5cf06d9a966e1ababf6225ee1392dd964
NI57721 <NI57721@users.noreply.github.com>
2023-10-29 11:31:15 +0900

    Exclude repositories whose names end with "-sandbox" in "girm"

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -101,7 +101,7 @@ abbr -a gishl git stash list
 abbr -a --set-cursor gishs git stash show -p stash@{%}
 abbr -a gipf git push --force-with-lease --force-if-includes
 abbr -a gipu git push --set-upstream origin \(git branch --show-current\)
-abbr -a girm git remind status --short
+abbr -a girm git remind status --short \| grep -ve "-sandbox\\\$"
 
 # for Docker
 abbr docl docker container ls
