5c21697678655ab47a4f3bab71083b985c4f222e
NI57721 <NI57721@users.noreply.github.com>
2024-03-02 09:45:01 +0900

    Add an abbreviation and an alias to enable the monitor of ThinkPad

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -57,7 +57,8 @@ abbr -a .. cd ..
 abbr -a ... cd ../..
 abbr -a von sudo ipsec up interlink
 abbr -a vof sudo ipsec down interlink
-abbr -a om swaymsg output eDP-1 disable
+abbr -a em swaymsg output eDP-1 enable
+abbr -a dm swaymsg output eDP-1 disable
 abbr -a cdv cd $XDG_DATA_HOME/dein/repos/github.com/
 abbr -a bye systemctl poweroff
 abbr -a later systemctl reboot
