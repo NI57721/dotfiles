d5044d9dda6a48d855220930a0e4c04bcb59238d
NI57721 <NI57721@users.noreply.github.com>
2025-07-18 19:10:23 +0900

    Use -k option of wtype

--- .config/xremap/scripts/clone-from-github.sh
+++ .config/xremap/scripts/clone-from-github.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-wtype -M ctrl l -s 50 -P insert -p insert -P escape -p escape
+wtype -M ctrl l -s 50 -k insert -k escape
 swaymsg [app_id="^org\.wezfurlong\.wezterm$"] focus
 wezterm cli spawn -- fish -c "cd ~/dev; git clone git@github.com:$(wl-paste | sed -r "s@https://github\.com/([^/]+/[^/]+).*@\1@")"
 
--- .config/xremap/scripts/open-with-vim.sh
+++ .config/xremap/scripts/open-with-vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-wtype -M ctrl l -s 50 -P insert -p insert -P escape -p escape
+wtype -M ctrl l -s 50 -k insert -k escape
 swaymsg [app_id="^org\.wezfurlong\.wezterm$"] focus
 wezterm cli spawn -- vim $(wl-paste)
 
