a1fef5e0a98126992ab519e7bf5018140ef0270e
NI57721 <NI57721@users.noreply.github.com>
2025-07-18 20:54:38 +0900

    Stop using black magic... as best I can

--- .config/xremap/scripts/clone-from-github.sh
+++ .config/xremap/scripts/clone-from-github.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -eu
 
+MAIN_WEZTERM_PID=$(swaymsg -t get_tree | jq '.. | objects | select(.app_id? == "MainTerminal") | .pid')
+export WEZTERM_UNIX_SOCKET=/run/user/$(id -u)/wezterm/gui-sock-$MAIN_WEZTERM_PID
+
 wtype -M ctrl l -s 50 -k insert -k escape
-swaymsg [app_id="^MainTerminal$"] focus
 user=$(wl-paste | sed -r "s@https://github\.com/([^/]+).*@\1@")
 repository=$(wl-paste | sed -r "s@https://github\.com/[^/]+/([^/]+).*@\1@")
-wtype -M alt o -m alt -s 100 cd \~/dev\; git clone git@github.com:$user/$repository\; \~/dev/$repository -k return
+
+wezterm cli spawn -- git clone git@github.com:$user/$repository ~/dev/$repository
+wezterm cli spawn --cwd ~/dev/$repository -- fish
+swaymsg [app_id="^MainTerminal$"] focus
 
--- .config/xremap/scripts/open-with-vim.sh
+++ .config/xremap/scripts/open-with-vim.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -eu
 
+MAIN_WEZTERM_PID=$(swaymsg -t get_tree | jq '.. | objects | select(.app_id? == "MainTerminal") | .pid')
+export WEZTERM_UNIX_SOCKET=/run/user/$(id -u)/wezterm/gui-sock-$MAIN_WEZTERM_PID
+
 wtype -M ctrl l -s 50 -k insert -k escape
+wezterm cli spawn -- vim $(wl-paste)
 swaymsg [app_id="^MainTerminal$"] focus
-wtype -M alt o -m alt -s 100 vim\  -M shift -k insert -m shift -s 50 -k return
 
