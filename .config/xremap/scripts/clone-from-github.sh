254d9c2b908b8c1d3ed7ca94e7c3937420916332
NI57721 <NI57721@users.noreply.github.com>
2025-08-01 16:46:42 +0900

    Exit when failing to get a user name and a repository name

--- .config/xremap/scripts/clone-from-github.sh
+++ .config/xremap/scripts/clone-from-github.sh
@@ -4,10 +4,14 @@ MAIN_WEZTERM_PID=$(swaymsg -t get_tree | jq '.. | objects | select(.app_id? == "
 export WEZTERM_UNIX_SOCKET=/run/user/$(id -u)/wezterm/gui-sock-$MAIN_WEZTERM_PID
 
 wtype -M ctrl l -s 50 -k insert -k escape
-user=$(wl-paste | sed -r "s@https://github\.com/([^/]+).*@\1@")
-repository=$(wl-paste | sed -r "s@https://github\.com/[^/]+/([^/]+).*@\1@")
+user=$(wl-paste | sed -r "s@https://github\.com/([^/?]+).*@\1@")
+repository=$(wl-paste | sed -r "s@https://github\.com/[^/]+/([^/?]+).*@\1@")
 
-wezterm cli spawn -- git clone git@github.com:$user/$repository ~/dev/$repository
+if [ -z "$user" ] || [ -z "$repository" ]; then
+  exit
+fi
+
+git clone git@github.com:$user/$repository ~/dev/$repository &
 wezterm cli spawn --cwd ~/dev/$repository -- fish
 swaymsg [app_id="^MainTerminal$"] focus
 
